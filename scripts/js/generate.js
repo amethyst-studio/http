15b94515ee4210ac846cb058ab0a618f18a800c0
Samuel J Voeller <12182390+xCykrix@users.noreply.github.com>
2022-02-02 17:48:22 +0100

    chore: force trunk fixes to repo

--- scripts/js/generate.js
+++ scripts/js/generate.js
@@ -1,6 +1,6 @@
-const { readdirSync, readFileSync, writeFileSync } = require('fs')
-const { resolve, basename } = require('path')
-const { readdir } = require('fs').promises
+const { readdirSync, readFileSync, writeFileSync } = require("fs");
+const { resolve, basename } = require("path");
+const { readdir } = require("fs").promises;
 
 /**
  * Performs a deep walk of the given folder and returns an array of files.
@@ -9,56 +9,63 @@ const { readdir } = require('fs').promises
  * @returns {string[]} An array of files.
  */
 async function getFiles(folder) {
-  const dirents = await readdir(folder, { withFileTypes: true })
-  const files = await Promise.all(dirents.map((dirent) => {
-    const res = resolve(folder, dirent.name)
-    return dirent.isDirectory() ? getFiles(res) : res
-  }))
-  return files.flat()
+  const dirents = await readdir(folder, { withFileTypes: true });
+  const files = await Promise.all(
+    dirents.map((dirent) => {
+      const res = resolve(folder, dirent.name);
+      return dirent.isDirectory() ? getFiles(res) : res;
+    })
+  );
+  return files.flat();
 }
 
 /**
-* Performs a deep merge of objects and returns new object. Does not modify
-* objects (immutable) and merges arrays via concatenation.
-*
-* @param {...object} objects - The object(s) to merge.
-* @returns {object} The new object after merging.
-*/
+ * Performs a deep merge of objects and returns new object. Does not modify
+ * objects (immutable) and merges arrays via concatenation.
+ *
+ * @param {...object} objects - The object(s) to merge.
+ * @returns {object} The new object after merging.
+ */
 function merge(...objects) {
-  const isObject = obj => obj && typeof obj === 'object'
+  const isObject = (obj) => obj && typeof obj === "object";
 
   return objects.reduce((prev, obj) => {
-    Object.keys(obj).forEach(key => {
-      const pVal = prev[key]
-      const oVal = obj[key]
+    Object.keys(obj).forEach((key) => {
+      const pVal = prev[key];
+      const oVal = obj[key];
 
       if (Array.isArray(pVal) && Array.isArray(oVal)) {
-        prev[key] = pVal.concat(...oVal)
+        prev[key] = pVal.concat(...oVal);
+      } else if (isObject(pVal) && isObject(oVal)) {
+        prev[key] = merge(pVal, oVal);
+      } else {
+        prev[key] = oVal;
       }
-      else if (isObject(pVal) && isObject(oVal)) {
-        prev[key] = merge(pVal, oVal)
-      }
-      else {
-        prev[key] = oVal
-      }
-    })
+    });
 
-    return prev
-  }, {})
+    return prev;
+  }, {});
 }
 
 // Read and Generate from FS
 
 async function make() {
-  const upstreamContent = readFileSync(resolve(__dirname, '../../sys/data/_.msf.json'))
-  const jsonContent = readFileSync(resolve(__dirname, '../../conf.d/inject.json'))
+  const upstreamContent = readFileSync(
+    resolve(__dirname, "../../sys/data/_.msf.json")
+  );
+  const jsonContent = readFileSync(
+    resolve(__dirname, "../../conf.d/inject.json")
+  );
 
-  const upstream = JSON.parse(upstreamContent)
-  const inject = JSON.parse(jsonContent)
+  const upstream = JSON.parse(upstreamContent);
+  const inject = JSON.parse(jsonContent);
 
-  const final = merge(upstream, inject)
+  const final = merge(upstream, inject);
 
-  writeFileSync(resolve(__dirname, '../../sys/data/_r.msf.json'), JSON.stringify(final, null, 2))
+  writeFileSync(
+    resolve(__dirname, "../../sys/data/_r.msf.json"),
+    JSON.stringify(final, null, 2)
+  );
 }
 
-make()
+make();
